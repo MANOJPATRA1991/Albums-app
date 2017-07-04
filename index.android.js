3c41b86f4a418b229820d5ca2f2cf3dd5575d981
MANOJ PATRA <patra.manoj0@gmail.com>
2017-07-04 23:05:12 +0530

    Initial Commit

---
+++ index.android.js
@@ -0,0 +1,11 @@
+// Index.android.js - place code in here for android
+
+// Import a library to help create a component
+import React from 'react';
+import ReactNative from 'react-native';
+
+
+// Create a component
+
+
+// Render it to the device

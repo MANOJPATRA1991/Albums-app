1b6e6baa2333f58c8a59d863c873fce25d02b47f
MANOJ PATRA <patra.manoj0@gmail.com>
2017-07-05 19:05:11 +0530

    Add an AlbumList component

---
+++ src/components/AlbumList.js
@@ -0,0 +1,16 @@
+// Import libraries for making a component
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+// Make the AlbumList component
+class AlbumList extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Album List</Text>
+            </View>
+        );
+    }
+}
+// Make the component available to other parts of the app
+export default AlbumList;

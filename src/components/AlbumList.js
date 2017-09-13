cac9b90e600a02454bb06cd89773b51b99235e42
MANOJ PATRA <patra.manoj0@gmail.com>
2017-09-13 17:55:24 +0530

    Add AlbumList

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -4,7 +4,27 @@ import { View, Text } from 'react-native';
 
 // Make the AlbumList component
 class AlbumList extends Component {
+    constructor() {
+        super();
+        // class level property
+        /* 
+            this property will be available for use
+            when the component first renders
+        */
+        this.state = { albums: [] };
+    }
+
+    componentWillMount() {
+        // ASYNC HTTP Request to get albums from the API.
+        // eslint-disable-next-line
+        fetch('https://rallycoding.herokuapp.com/api/music_albums')
+        .then(response => response.json())
+        .then(data => this.setState({ albums: data }));
+    }
+
     render() {
+        console.log(this.state);
+
         return (
             <View>
                 <Text>Album List</Text>

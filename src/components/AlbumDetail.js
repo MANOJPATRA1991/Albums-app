3a4ab925f85dd3039ce26543d382c98ff6cb4fd8
MANOJ PATRA <patra.manoj0@gmail.com>
2017-09-28 11:13:52 +0530

    Update album detail component

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { Text } from 'react-native';
+import Card from './Card';
 
 class AlbumDetail extends Component {
     render() {
         return (
-            <View>
-                <Text>
-                    {this.props.album.title}
-                </Text>
-            </View>
+            <Card>
+                <Text>{this.props.album.title}</Text>
+            </Card>
         );
     }
 }
